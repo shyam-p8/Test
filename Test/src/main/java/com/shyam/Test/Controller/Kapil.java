22474e8a83949c73205e53bb60a296a40bd4611c
Kapil <bhadauria.kapil1919@gamil.com>
2023-07-12 17:18:55 +0530

    commint from kapil

---
+++ Test/src/main/java/com/shyam/Test/Controller/Kapil.java
@@ -0,0 +1,21 @@
+package com.shyam.Test.Controller;
+
+import com.shyam.Test.Service.AuthService;
+
+public class Kapil {
+
+    private AuthService authService;
+
+    public String saveData()
+    {
+
+        //write code here
+
+        return "ok";
+
+
+    }
+
+}
+
+
